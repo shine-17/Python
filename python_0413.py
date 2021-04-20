75c788ce2edc1a3f84b5b1a7e18a5c0165ddf5a6
dohun <shine_17@naver.com>
2021-04-20 20:59:15 +0900

    0420 python

--- python_0413.py
+++ python_0413.py
@@ -240,3 +240,139 @@ a = b = [1,2,3]
 a[1]=4
 print(b)
 #a와 b는 같은 레퍼런스를 참조하기 때문에(동일한 [1,2,3]이라는 리스트 객체를 가리킴) 값을 변경해도 똑같이 변경된다.
+
+
+money = True
+if money:
+    print("택시를 타고 가라")
+else:
+    print("걸어 가라")
+
+print(1 in [1,2,3])
+print(1 not in [1,2,3])
+
+#조건문에서 아무 일도 하지 않게 설정
+pocket = ['paper','money','cellphone']
+if 'money' in pocket:
+    pass
+else:
+    print('카드를 꺼내라')
+
+card = True
+if 'money' in pocket:
+    print("택시를 타고 가라")
+elif card:
+    print("택시를 타고 가라")
+else:
+    print("걸어가라")
+
+score=59
+
+if score>=60:
+    message = "success"
+else:
+    message = "failure"
+
+print(message)
+
+treeHit=0
+while treeHit < 10:
+    treeHit = treeHit + 1
+    print("나무를 %d번 찍었습니다." % treeHit)
+    if treeHit==10:
+        print("나무 넘어갑니다.")
+
+prompt = """
+... 1. Add
+... 2. Del
+... 3. List
+... 4. Quit
+...
+... Enter number : """
+
+
+number = 0
+#while number != 4:
+    #print(prompt)
+    #number = int(input())
+    
+
+coffee = 10
+money = 300
+while money:
+    print("돈을 받았으니 커피를 줍니다")
+    coffee = coffee - 1
+    print("남은 커피의 양은 %d개입니다." % coffee)
+    if coffee == 0:
+        print("커피가 다 떨어졌습니다. 판매를 중지합니다.")
+        break
+
+#for문
+test_list = ['one','two','three']
+for i in test_list:
+    print(i)
+
+
+a = [(1,2),(3,4),(5,6)]
+for (first, last) in a:
+    print(first+last)
+
+marks = [90, 25, 67, 45, 80]
+
+number = 0
+for mark in marks:
+    number = number + 1
+    if mark >= 60:
+        print("%d번 학생은 합격입니다." % number)
+    else:
+        print("%d번 학생은 불합격입니다." % number)
+
+add = 0
+for i in range(1, 11):
+    add = add + i
+
+print(add)
+
+for i in range(2,10):
+    for j in range(1,10):
+        print(i*j, end=" ")
+    print('')
+
+a = [1,2,3,4]
+result = [num * 3 for num in a]
+print(result)
+
+#3장
+#연습문제 1번
+a = "Life is too short, you need python"
+
+if "wife" in a: print("wife")
+elif "python" in a and "you" not in a: print("python")
+elif "shirt" not in a: print("shirt")
+elif "need" in a: print("need")
+else: print("none")
+
+#정답 shirt
+
+#연습문제 2번
+#while문을 사용해 1부터 1000까지의 자연수 중 3의 배수의 합을 구해 보자.
+i=0
+num=0
+while i<=1000:
+    i = i+1
+    
+    if(i%3==0):
+        num = num + i
+
+print(num)
+
+
+#연습문제 3번
+#while문을 사용하여 다음과 같이 별(*)을 표시하는 프로그램 작성
+
+for i in range(1,5):
+    for j in range(1,5):
+        print("*")
+
+
+
